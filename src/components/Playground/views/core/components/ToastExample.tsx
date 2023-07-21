e6693f73ae6e48ea0717d60a84fb3a3dba7d8626
Gonzalo Uceda <gonzalo.uceda@devo.com>
2023-07-21 12:51:34 +0200

    chore: fix build

--- src/components/Playground/views/core/components/ToastExample.tsx
+++ src/components/Playground/views/core/components/ToastExample.tsx
@@ -1,12 +1,11 @@
-import { ToastStatus } from '@devoinc/genesys-ui';
 import {
   Button,
   Flex,
   HFlex,
   toast,
   ToastContainer,
-  ToastProps,
 } from '@devoinc/genesys-ui';
+import { ToastStatus } from '@devoinc/genesys-ui/dist/types/src/components/Toast/declarations';
 
 const lorem01 =
   'Phasellus hendrerit lacus ac nisl laoreet vulputate. Vestibulum vitae enim magna. Integer malesuada lacus et urna pellentesque, quis hendrerit purus sodales. Phasellus sagittis leo nec ornare congue. Quisque blandit tristique turpis, eget pretium orci bibendum sed. Aliquam imperdiet ultrices scelerisque. Vestibulum sit amet lorem cursus, auctor purus et, pretium velit.';
