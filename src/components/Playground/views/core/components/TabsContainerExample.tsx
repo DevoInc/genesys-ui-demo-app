2e757d05e44a9644c12bd3e14b89079a0d343efd
Gonzalo Uceda <gonzalo.uceda@devo.com>
2023-10-27 09:40:22 +0200

    build: fix error

--- src/components/Playground/views/core/components/TabsContainerExample.tsx
+++ src/components/Playground/views/core/components/TabsContainerExample.tsx
@@ -9,23 +9,23 @@ export const TabsContainerExample = () => {
       <Tabs.Item
         icon='gi-check_thick'
         label='Tiny'
-        onTabClick={() => setActiveTab(0)}
+        onClick={() => setActiveTab(0)}
         state={activeTab === 0 ? 'selected' : undefined}
       />
       <Tabs.Item
         icon='gi-check_thick'
         label='Tab with a very very long title'
-        onTabClick={() => setActiveTab(1)}
+        onClick={() => setActiveTab(1)}
         state={activeTab === 1 ? 'selected' : undefined}
       />
       <Tabs.Item
         label='Normal tab'
-        onTabClick={() => setActiveTab(2)}
+        onClick={() => setActiveTab(2)}
         state={activeTab === 2 ? 'selected' : undefined}
       />
       <Tabs.Item
         label='Disabled tab'
-        onTabClick={() => setActiveTab(3)}
+        onClick={() => setActiveTab(3)}
         state='disabled'
       />
     </Tabs>
